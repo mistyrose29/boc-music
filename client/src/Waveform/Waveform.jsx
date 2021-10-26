d43fff3cdb99b20206a48e3fd1b27cbec032cbff
Sung <jajh90@gmail.com>
2021-10-26 07:29:14 -0500

    fixed minor bugs, updated audio controller, added playing time display, basic css

--- client/src/Waveform/Waveform.jsx
+++ client/src/Waveform/Waveform.jsx
@@ -15,7 +15,7 @@ const formWaveSurferOptions = ref => ({
   partialRender: true
 });
 
-export default function Waveform({ url }) {
+export default function Waveform({ url, id, tracks, setSelectedTrack }) {
   const waveformRef = useRef(null);
   const wavesurfer = useRef(null);
   const [playing, setPlay] = useState(false);
@@ -30,10 +30,30 @@ export default function Waveform({ url }) {
 
     wavesurfer.current.load(url);
 
-    wavesurfer.current.on("ready", function() {
+    wavesurfer.current.on("ready", function () {
       if (wavesurfer.current) {
         wavesurfer.current.setVolume(volume);
         setVolume(volume);
+
+        // display total playing time
+        const total = wavesurfer.current.getDuration().toFixed();
+        const totalMinutes = Math.floor(total / 60)
+        let totalSeconds = total % 60
+        if ((totalSeconds/10) < 1) {
+          totalSeconds = `0${totalSeconds}`
+        }
+        document.getElementById('time-total').innerText = ` ${totalMinutes}:${totalSeconds}`;
+
+        // update & display current playing time
+        wavesurfer.current.on('audioprocess', () => {
+          const current = wavesurfer.current.getCurrentTime().toFixed();
+          const currentMinutes = Math.floor(current/60)
+          let currentSeconds = current % 60
+          if ((currentSeconds/10) < 1) {
+            currentSeconds = `0${currentSeconds}`
+          }
+          document.getElementById('time-current').innerText = ` ${currentMinutes}:${currentSeconds} `;
+        });
       }
     });
 
@@ -41,36 +61,79 @@ export default function Waveform({ url }) {
   }, [url]);
 
   const handlePlayPause = () => {
-    setPlay(!playing);
-    wavesurfer.current.playPause();
+    if (wavesurfer.current.isReady === true) {
+      setPlay(!playing);
+      wavesurfer.current.playPause();
+    }
   };
+
+  const previous = (id) => {
+    let prevId = id - 1;
+    // loop back to the end of the playlist
+    if (tracks[prevId] === undefined) {
+      prevId = tracks.length - 1;
+    }
+    setSelectedTrack(tracks[prevId])
+  }
+
+  const next = (id) => {
+    let nextId = id + 1;
+    // loop back to the start of the playlist
+    if (tracks[nextId] === undefined) {
+      nextId = 0;
+    }
+    setSelectedTrack(tracks[nextId])
+  }
 
   const onVolumeChange = e => {
     const { target } = e;
     const newVolume = +target.value;
 
-    if (newVolume) {
-      setVolume(newVolume);
-      wavesurfer.current.setVolume(newVolume || 1);
-    }
+    setVolume(newVolume);
+    wavesurfer.current.setVolume(newVolume);
   };
 
+  const mute = () => {
+    if (wavesurfer.current.isReady) {
+      wavesurfer.current.toggleMute();
+      // update volume indicator on mute toggle
+      let volIndicator = document.getElementById("volume");
+      if (wavesurfer.current.isMuted) {
+        volIndicator.value = 0;
+        setVolume(0);
+      } else {
+        let prevVol = Math.round(wavesurfer.current.savedVolume * 1000) / 1000
+        volIndicator.value = prevVol;
+        setVolume(prevVol)
+      }
+    }
+  }
+
   return (
-    <div>
+    <div className="waveform-container">
       <div id="waveform" ref={waveformRef} />
+      <div>
+        Playing Time
+        <span id="time-current"> 0:00 </span> / <span id="time-total">0:00</span>
+      </div>
       <div className="controls">
         <button onClick={handlePlayPause}>{!playing ? "Play" : "Pause"}</button>
+        <button onClick={() => { previous(id) }}>Previous</button>
+        <button onClick={() => { next(id) }}>Next</button>
+        <button onClick={mute}>Mute</button>
         <input
           type="range"
           id="volume"
           name="volume"
-          min="0.01"
-          max="1"
+          min="0"
+          max="1.0"
           step=".025"
           onChange={onVolumeChange}
           defaultValue={volume}
         />
-        <label htmlFor="volume">Volume</label>
+        <label htmlFor="volume">
+          {Math.round(volume * 100)}% Volume
+        </label>
       </div>
     </div>
   );
