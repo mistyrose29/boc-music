bfe756e42ce75233f799fb9593c40cd437174cb1
Quinn Lima <quinnlima@quinns-air.gateway.sonic.net>
2021-10-28 19:05:22 -0700

    router added

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,26 +3,79 @@ import ReactDOM from 'react-dom';
 //import Waveform from './Waveform/Waveform.jsx'
 import WaveformApp from './Waveform/WaveformApp.jsx';
 import Layers from './Layering/Layer.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      load: false
 
 
     };
+    this.login = this.login.bind(this);
   }
+
+  login() {
+    this.setState({
+      load: true
+    })
+  }
+
+
 
   render() {
-    return (
-      <div>BOC
-        <WaveformApp />
-        <hr />
-        <Layers />
-      </div>
-    );
+    
+      if (this.state.load) {
+        return (
+          <Router>
+            <Switch>
+              <Route path="/login">
+                <Redirect to={"/home"}/>
+              </Route>
+              <Route path="/" exact>
+                <Redirect to={"/home"}/>
+              </Route>
+              <Route path="/home">
+                  <button  onClick={() => {
+                    this.history.push(`/layers`);
+                  }}>layers</button>
+                  <button onClick={() => {
+                  this.history.push('/waveform');
+                }}>waveform</button>
+              </Route>
+              <Route path="/layers">
+                <Layers/>
+              </Route>
+              <Route path="/waveform">
+              <WaveformApp />
+              </Route>
+            </Switch>
+          </Router>
+        )
+      } else {
+        return (
+          <Router>
+            <Switch>
+              <Route path="/">
+                <button onClick = {this.login}> login </button>
+              </Route>
+            </Switch>
+          </Router>
+        )
+      }
+      
+ 
+    
   }
 
-}
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
