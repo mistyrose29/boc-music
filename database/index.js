dbd04c33e9daf00bd9c7ef2a997f4a001d0e94e3
Jason <jmolleru@asu.edu>
2021-10-30 22:01:40 -0700

    added anonymous image for anonymous users

--- database/index.js
+++ database/index.js
@@ -55,7 +55,7 @@ var uiConfig = {
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
   tosUrl: '<your-tos-url>',
