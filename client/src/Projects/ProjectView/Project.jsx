d737445fbbcdb427af3fa30197cae946a45bbde6
Sung <jajh90@gmail.com>
2021-11-05 02:21:16 -0500

    finally done

--- client/src/Projects/ProjectView/Project.jsx
+++ client/src/Projects/ProjectView/Project.jsx
@@ -42,17 +42,21 @@ class Project extends React.Component {
     wavesurfers.push(WS)
     this.setState({
       wavesurfers: wavesurfers
-    }, () => console.log(this.state.wavesurfers))
+    })
   }
 
   async mix(soundSources) {
-    const audioContext = new AudioContext();
+    // display mixing status and disable click
+    let mixBtn = document.getElementById('mix')
+    mixBtn.innerText = 'Mixing and compressing...'
+    mixBtn.disabled = true;
 
     let maxDuration = 0;
-    let maxChannels = 2;
+    let maxChannels = 0;
 
+    // Find maximum duration and channels from the sources
     soundSources.map((src) => {
-      const duration = src.getDuration()
+      const duration = src.backend.buffer.duration
       const channels = src.backend.buffer.numberOfChannels
       if (maxChannels < channels) {
         maxChannels = channels
@@ -61,65 +65,112 @@ class Project extends React.Component {
         maxDuration = duration + 1
       }
     })
+    // Init offlineAudioContext
     let offlineCtx = new OfflineAudioContext({
       numberOfChannels: maxChannels,
-      length: 48000 * maxDuration,
-      sampleRate: 48000
+      length: 44100 * maxDuration,
+      sampleRate: 44100,
     });
-    let mixedAudio = audioContext.createMediaStreamDestination();
-
+    // Create buffer source, then set its buffer to the AudioBuffer
     soundSources.map((src) => {
       let source = offlineCtx.createBufferSource();
       source.buffer = src.backend.buffer
-      source.connect(offlineCtx.destination)
+
+      // Create Compressor Node
+      var compressor = offlineCtx.createDynamicsCompressor();
+
+      compressor.threshold.setValueAtTime(-5, offlineCtx.currentTime); // set value btw -100 and 0
+      compressor.knee.setValueAtTime(15, offlineCtx.currentTime); // set value btw 0 and 40
+      compressor.ratio.setValueAtTime(3, offlineCtx.currentTime); // set value btw 1 and 20
+      compressor.attack.setValueAtTime(.005, offlineCtx.currentTime); // set value btw 0 and 1
+      compressor.release.setValueAtTime(0.15, offlineCtx.currentTime); // set value btw 0 and 1
+
+      // Connect nodes to destination
+      source.connect(compressor)
+      compressor.connect(offlineCtx.destination)
+      // Play through the audio buffer to process audio
       source.start()
     })
+    // Render the processed audio to layer all AudioBuffers to one AudioBuffer (rendered)
+    offlineCtx.startRendering().then((rendered) => {
+      make_download(rendered, offlineCtx.length)
+    })
 
-    offlineCtx.startRendering().then((rend) => {
-      let chunks = [];
-      let blob;
-      let song = audioContext.createBufferSource();
-      song.buffer = rend;
-      song.connect(mixedAudio)
-      song.connect(audioContext.destination)
+    // Helper functions
+    function make_download(audioBuffer, total_samples) {
+      // Set sample length and rate
+      let duration = audioBuffer.duration,
+        rate = audioBuffer.sampleRate,
+        offset = 0;
 
-      const recorder = new MediaRecorder(mixedAudio.stream);
-      recorder.start(0)
-      song.start(0)
+      // Convert audioBuffer to .wav file and assign URL
+      let new_file = URL.createObjectURL(bufferToWave(audioBuffer, total_samples));
 
-      setTimeout(() => {
-        console.log('stopped')
-        song.stop()
-        recorder.stop()
-      }, maxDuration*1000);
-      recorder.ondataavailable = function (event) {
-        if (event.data.size > 0) {
-          chunks.push(event.data);
+      // Set up downloadable link
+      let download_link = document.getElementById("download");
+      download_link.href = new_file;
+      let name = "download.wav";
+      download_link.innerText = name;
+      download_link.download = name;
+      // bring back the mix button
+      mixBtn.innerText = 'mix'
+      mixBtn.disabled = false;
+    }
+
+    // Convert AudioBuffer to a Blob using WAVE representation
+    function bufferToWave(audioBuffer, len) {
+      let numOfChan = audioBuffer.numberOfChannels,
+        length = len * numOfChan * 2 + 44,
+        buffer = new ArrayBuffer(length),
+        view = new DataView(buffer),
+        channels = [], i, sample,
+        offset = 0,
+        pos = 0;
+
+      // write WAVE header
+      setUint32(0x46464952);                         // "RIFF"
+      setUint32(length - 8);                         // file length - 8
+      setUint32(0x45564157);                         // "WAVE"
+
+      setUint32(0x20746d66);                         // "fmt " chunk
+      setUint32(16);                                 // length = 16
+      setUint16(1);                                  // PCM (uncompressed)
+      setUint16(numOfChan);
+      setUint32(audioBuffer.sampleRate);
+      setUint32(audioBuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+      setUint16(numOfChan * 2);                      // block-align
+      setUint16(16);                                 // 16-bit (hardcoded in this demo)
+
+      setUint32(0x61746164);                         // "data" - chunk
+      setUint32(length - pos - 4);                   // chunk length
+
+      // write interleaved data
+      for (i = 0; i < audioBuffer.numberOfChannels; i++)
+        channels.push(audioBuffer.getChannelData(i));
+
+      while (pos < length) {
+        for (i = 0; i < numOfChan; i++) {             // interleave channels
+          sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
+          sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767) | 0; // scale to 16-bit signed int
+          view.setInt16(pos, sample, true);          // write 16-bit sample
+          pos += 2;
         }
-      };
-
-      let btn = document.getElementById('mix')
-      btn.disabled = true
-      btn.innerHTML = 'Mixing in progress...'
-
-      recorder.onstop = function (event) {
-        console.log('finished mixing')
-        blob = new Blob(chunks, {
-          "type": "audio/ogg; codecs=opus"
-        });
-
-        const audioDownload = URL.createObjectURL(blob);
-        let description = 'download'
-
-        let downloadLink = document.getElementById('download')
-        downloadLink.download = description + "." + blob.type.replace(/.+\/|;.+/g, "");
-        downloadLink.href = audioDownload
-        downloadLink.innerHTML = downloadLink.download;
-        btn.disabled = false
-        btn.innerHTML = 'Mix'
+        offset++                                     // next source sample
       }
 
-    })
+      // create Blob
+      return new Blob([buffer], { type: "audio/wav" });
+
+      function setUint16(data) {
+        view.setUint16(pos, data, true);
+        pos += 2;
+      }
+
+      function setUint32(data) {
+        view.setUint32(pos, data, true);
+        pos += 4;
+      }
+    }
   }
 
   reload() {
@@ -195,17 +246,19 @@ class Project extends React.Component {
             );
           })}
         </>
-        <div id='mixing'></div>
-        <button
-          id='mix'
-          onClick={() => this.mix(this.state.wavesurfers)}
-        >
-          mix
-        </button>
-        <a id="download"></a>
+        <div id='mixing'>
+          <button
+            id='mix'
+            onClick={() => this.mix(this.state.wavesurfers)}
+          >
+            mix
+          </button>
+          <a id="download"></a>
+        </div>
       </div>
     );
   }
 }
 
 export default Project;
+
