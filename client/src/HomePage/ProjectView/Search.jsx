b131c87bc5fc83a1f3f360a979d5e5cb3940f884
Jason <jmolleru@asu.edu>
2021-11-13 09:24:41 -0700

    added spacing and changed buttons to outline-light on Search component

--- client/src/HomePage/ProjectView/Search.jsx
+++ client/src/HomePage/ProjectView/Search.jsx
@@ -3,64 +3,72 @@ import { useState } from 'react';
 import { Button, Modal, Tabs, Tab, Form, Card, Dropdown } from 'react-bootstrap';
 
 const Search = ({ setFilters, searchTab }) => {
-    const [key, setKey] = useState('mine');
-    const [search, setSearch] = useState('');
-    const [dropDown, setDropDown] = useState('Mine')
+  const [key, setKey] = useState('mine');
+  const [search, setSearch] = useState('');
+  const [dropDown, setDropDown] = useState('Mine');
 
 
-    const handleSearch = () => {
-      let keyFilter = dropDown.toLowerCase()
-      setFilters({
-        key: keyFilter,
-        query: search
-      });
-    };
-  
-    const handleChange = (event) => {
-      let term = event.target.value;
-      setSearch(term);
-    };
+  const handleSearch = () => {
+    let keyFilter = dropDown.toLowerCase();
+    setFilters({
+      key: keyFilter,
+      query: search
+    });
+  };
 
-    const changeDropDown = (event) => {
-      setDropDown(event.target.innerText);
-    }
-  
+  const handleChange = (event) => {
+    let term = event.target.value;
+    setSearch(term);
+  };
+
+  const changeDropDown = (event) => {
+    setDropDown(event.target.innerText);
+  };
+
   if (searchTab) {
     return (
-      <div style={{ margin: '0 10px 10px 10px'}}>
-      <Card className='card-shadow'>
-      <Card.Header>
-      </Card.Header>
-      <Card.Body>
+      <div style={{ margin: '0 10px 10px 10px' }}>
+        <Card className='card-shadow'>
+          <Card.Body>
             <Form>
-            <Form.Group className="mb-3-top" controlId="formBasicEmail">
-              <Form.Control type="title" placeholder="Search Project Title" onChange={handleChange} />
-            </Form.Group>
-          </Form>
-          <div className = "search-bar-bottom">
-          <Dropdown>
-          <Dropdown.Toggle variant="primary" id="dropdown-basic" style = {{margin: "0 10px 0 0"}}>
-            {dropDown}
-          </Dropdown.Toggle>
-        <Dropdown.Menu >
-          <Dropdown.Item onClick = {changeDropDown}>All</Dropdown.Item>
-          <Dropdown.Item onClick = {changeDropDown}>Mine</Dropdown.Item>
-          <Dropdown.Item onClick = {changeDropDown}>Shared</Dropdown.Item>
-        </Dropdown.Menu>
-        </Dropdown>
-          
-          <Button variant="primary" onClick={handleSearch}>
-            Search
-          </Button>
-          </div>
-        </Card.Body>
-      </Card>
+              <Form.Group
+                className="mb-3"
+                controlId="formBasicEmail">
+                <Form.Control type="title" placeholder="Search Project Title" onChange={handleChange} />
+              </Form.Group>
+            </Form>
+            <div className="search-bar-bottom">
+              <Dropdown>
+                <Dropdown.Toggle
+                  size='sm'
+                  variant="outline-light"
+                  id="dropdown-basic"
+                  style={{
+                    margin: "0 10px 0 0"
+                  }}>
+                  {dropDown}
+                </Dropdown.Toggle>
+                <Dropdown.Menu >
+                  <Dropdown.Item onClick={changeDropDown}>All</Dropdown.Item>
+                  <Dropdown.Item onClick={changeDropDown}>Mine</Dropdown.Item>
+                  <Dropdown.Item onClick={changeDropDown}>Shared</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Button
+                size='sm'
+                variant="outline-light"
+                onClick={handleSearch}>
+                Search
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
       </div>
     );
   } else {
-    
-    return null
+    return null;
   }
-  };
-  
-  export default Search;
+};
+
+export default Search;
